7295d5409b194d9b3df351846d3183b2af8c48df
wheelerwilliams <whlrwllms@gmail.com>
2022-09-02 04:21:20 -0400

    Final updates

--- assets/custom.js
+++ assets/custom.js
@@ -1,29 +1,55 @@
 var bundle_step = 0;
 var current_fs, next_fs;
-
+var add_to_cart = '';
+var category = '';
+  
 $(".bundle-start").click(function(){
   current_fs = $(this).parents("section");
   next_fs = $("#progressbar").parents("section").next();
   $("#progressbar").show()
-  next_fs.show();
-  current_fs.hide();
+  next_fs.addClass("active-section").show();
+  current_fs.removeClass("active-section").hide();
 })
 
-$(".bundle-next").click(function(){
+$(".section-gallery.bundle-section .bundle-next").click(function(){
+  $(".active-section .gallery__item.active").each(function(){
+    category += $(this).attr("category") + '&';
+  });
+  console.log("category: ", category);
+  
   bundle_step += 1;
   current_fs = $(this).parents("section");
   next_fs = $(this).parents("section").next();
   $("#progressbar li").eq($(".bundle-section").index(current_fs)).addClass("active");
-  next_fs.show();
-  current_fs.hide();
+  next_fs.addClass("active-section").attr("category", category).show();
+  current_fs.removeClass("active-section").hide();
 })
 
-
-$(".bundle-complete").click(function(){
-    addItemToCart( 39311716024414, 1, "1", "Months");
+$(".section-featured-collection.bundle-section .bundle-next").click(function(){
+  $(".active-section .product-block.active").each(function(){
+    add_to_cart += 'id=' + $(this).attr("variant-id") + '&quantity=1&';
+    addItemToCart( $(this).attr("variant-id"), 1);
+  });
+  console.log("query string: ", add_to_cart);
+  
+  bundle_step += 1;
+  current_fs = $(this).parents("section");
+  next_fs = $(this).parents("section").next();
+  $("#progressbar li").eq($(".bundle-section").index(current_fs)).addClass("active");
+  next_fs.addClass("active-section").show();
+  current_fs.removeClass("active-section").hide();
+  
 });
 
-function addItemToCart(variant_id, qty, frequency, unit_type) {
+$(".bundle-complete").click(function(){
+  $(".active-section .product-block.active").each(function(){
+    add_to_cart += 'id='+$(this).attr("variant-id") + '&quantity=1';
+  });
+  console.log(add_to_cart);
+  window.location.href = "/cart/add?" + add_to_cart;
+});
+
+function addItemToCart(variant_id, qty) {
   data = {
     "id": variant_id,
     "quantity": qty
@@ -34,15 +60,7 @@ function addItemToCart(variant_id, qty, frequency, unit_type) {
     url: '/cart/add.js',
     data: data,
     dataType: 'json',
-    success: function() { 
-      document.documentElement.dispatchEvent(new CustomEvent('cart:refresh', {
-        bubbles: true  
-     }));
-    }
   });
-  document.documentElement.dispatchEvent(new CustomEvent('cart:refresh', {
-      bubbles: true      
-   }));
 }
 
 
